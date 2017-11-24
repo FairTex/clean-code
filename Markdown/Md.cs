aa68967e9ea988f1fa369655730d3700d26e0c62
FairTex <makarov906@gmail.com>
2017-11-24 22:15:11 +0500

    fix

--- Markdown/Md.cs
+++ Markdown/Md.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Linq;
-using FluentAssertions;
-using NUnit.Framework;
 
 namespace Markdown
 {
@@ -23,20 +21,6 @@ namespace Markdown
                 return strongHandler.Handle(line);
             }).ToArray();
             return String.Join("", html);
-        }
-    }
-
-
-    [TestFixture]
-    public class Md_ShouldRender
-    {
-        [Test]
-        public void RenderToHtml_ShouldBeCorrect()
-        {
-            var md = new Md();
-            md.RenderToHtml("_em_ __strong__ _not __strong__ easy_  __of _italic_ anyway__")
-                .ShouldBeEquivalentTo(
-                    "<em>em</em> <strong>strong</strong> <em>not __strong__ easy</em>  __of <em>italic</em> anyway__");
         }
     }
 }
